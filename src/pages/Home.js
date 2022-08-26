4dd0aacf3743f53c38723c9df4f8a0ccc3054ca9
Shubham Vermani <shubhamlightning99@gmail.com>
2022-08-27 00:04:00 +0530

    Till BodyPart Component made

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
+
+const Home = () => {
+	//these states made here bcoz we want changes in state(when a diff body part is selected for eg) to reflect across all comps
+	const [exercises, setExercises] = useState([]);
+	const [bodyPart, setBodyPart] = useState('all');
+	return (
+		<Box>
+			<HeroBanner />
+			<SearchExercises
+				setExercises={setExercises}
+				bodyPart={bodyPart}
+				setBodyPart={setBodyPart}
+			/>
+			<Exercises
+				setExercises={setExercises}
+				bodyPart={bodyPart}
+				setBodyPart={setBodyPart}
+			/>
+		</Box>
+	);
+};
+
+export default Home;
