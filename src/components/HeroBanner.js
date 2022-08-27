79472e81d07bb70ce0166ff042ff574efff14fd0
Shubham Vermani <shubhamlightning99@gmail.com>
2022-08-28 03:17:14 +0530

    Name changes on site

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -10,7 +10,7 @@ const HeroBanner = () => (
 		p="20px"
 	>
 		<Typography color="#FF2625" fontWeight="600" fontSize="26px">
-			Fitness Club
+			Light Gym Club
 		</Typography>
 		<Typography
 			fontWeight={700}
